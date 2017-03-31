1a853659cd619555c7ba9ad70d50ffdf224ad60a
Brian Kubisiak <brian@kubisiak.com>
2017-03-31 11:47:45 -0700

    Add basic tests for bresenham line drawing.

---
+++ tests/bresenham/e.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <string.h>
+
+#include "bresenham.h"
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+    struct point origin = {0, 0};
+    struct point dst = {2, 0};
+    struct point output[4];
+    struct point answer[] = {{0, 0}, {1, 0}, {2, 0}};
+    size_t len;
+
+    len = bresenham(&origin, &dst, output, lengthof(output));
+
+    assert(len == lengthof(answer));
+    assert(memcmp(output, answer, sizeof(answer)) == 0);
+
+    return 0;
+}
---
+++ tests/bresenham/nw.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <string.h>
+
+#include "bresenham.h"
+#include "utils.h"
+
+int main(int argc, char *argv[])
+{
+    struct point origin = {0, 0};
+    struct point dst = {-2, 2};
+    struct point output[4];
+    struct point answer[] = {{0, 0}, {-1, 1}, {-2, 2}};
+    size_t len;
+
+    len = bresenham(&origin, &dst, output, lengthof(output));
+
+    assert(len == lengthof(answer));
+    assert(memcmp(output, answer, sizeof(answer)) == 0);
+
+    return 0;
+}
